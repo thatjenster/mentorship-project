e888d8828aaf99f955c87b853e1af3f5d917b782
markclapa-tophat <mark.clapa@tophatmonocle.com>
2022-08-10 14:11:17 -0400

    loads of tophat-prepared eslint configs

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
 
+import PropTypes from 'prop-types'
+import React from 'react'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />
+}
+
+MyApp.propTypes = {
+    Component: PropTypes.any,
+    pageProps: PropTypes.any,
 }
 
 export default MyApp
